e2fc281a03fa73891ea104cec7e416d55ef30b00
Serge <sajghaa@gmail.com>
2026-01-30 21:21:59 +0200

    BankServices class added

--- 02-bank-account-simulator/src/service/BankService.java
+++ 02-bank-account-simulator/src/service/BankService.java
@@ -1,5 +1,24 @@
 package service;
 
+import model.BankAccount;
 public class BankService {
-    
+    public void showAccount(BankAccount account){
+        System.out.println(account.getAccountInfo());
+    }
+
+    public void deposit(BankAccount account, double amount){
+        account.deposit(amount);
+        System.out.println("Deposit completed");
+    }
+
+    public void withdraw(BankAccount account, double amount){
+        boolean success = account.withdraw(amount);
+
+        if (success){
+            System.out.println("Withdrawal successful.");
+        } else {
+            System.out.println("Withdrawal failed: Insufficient balance.");
+        }
+    }
+
 }
