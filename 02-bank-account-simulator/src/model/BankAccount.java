061efa68ae4bba7cee9995602d4ca323218c2fc9
Serge <sajghaa@gmail.com>
2026-01-31 10:50:11 +0200

    We add enums for our AccountType in BankAccount and we add validation to make our app feels real

--- 02-bank-account-simulator/src/model/BankAccount.java
+++ 02-bank-account-simulator/src/model/BankAccount.java
@@ -1,14 +1,35 @@
 package model;
 
 public class BankAccount {
+
+    public enum AccountType{
+        CHECKING,
+        SAVINGS
+    }
+
     private String accountNumber;
+    private AccountType type;
     private String ownerName;
     private double balance;
 
     public BankAccount(String accountNumber, String ownerName, double balance){
+        // simple validation
+
+        if (accountNumber == null || accountNumber.isEmpty()){
+            throw new IllegalArgumentException("Account number cannot be empty");
+        }
+
+        if (ownerName == null || ownerName.isEmpty()){
+            throw new IllegalArgumentException("Owner name cannot be empty");
+        }
+
+        if (balance <0){
+            throw new IllegalArgumentException("Balance cannot be negative");
+        }
         this.accountNumber = accountNumber;
         this.ownerName = ownerName;
         this.balance = balance;
+        this.type = type;
     }
 
     public void deposit(double amount){
@@ -29,7 +50,10 @@ public class BankAccount {
         return balance;
     }
 
+    public AccountType getType(){
+        return type;
+    }
     public String getAccountInfo(){
-        return ownerName + " | Acc: " + accountNumber + "| Balance: " + balance;
+        return ownerName + " | Acc: " + accountNumber +"| Type: "+ type +"| Balance: " + balance;
     }
 }
