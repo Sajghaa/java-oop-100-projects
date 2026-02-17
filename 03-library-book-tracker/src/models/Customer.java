a05bde245cfbd52f8c1a24087fedeedba5d7f287
Serge <sajghaa@gmail.com>
2026-02-17 10:33:42 +0200

    Updated Customer.java

--- 03-library-book-tracker/src/models/Customer.java
+++ 03-library-book-tracker/src/models/Customer.java
@@ -1,8 +1,11 @@
 package models;
+
+import java.util.List;
+import java.util.ArrayList;
 public class Customer{
     private String customerId;
     private String name;
-
+    private List<Book> borrowedBooks;
     public Customer(String customerId, String name){
         if (customerId == null || customerId.trim().isEmpty()){
             throw new IllegalArgumentException("Customer ID required");
@@ -14,7 +17,24 @@ public class Customer{
 
         this.customerId = customerId;
         this.name = name;
+        this.borrowedBooks = new ArrayList<>();
     }
+
+     public void borrowBook(Book book) {
+        if (book.isAvailable()) {
+            book.borrowBook();
+            borrowedBooks.add(book);
+            System.out.println(name + " borrowed " + book.getTitle());
+        }
+    }
+
+   public void returnBook(Book book) {
+        if (borrowedBooks.contains(book)) {
+            book.returnBook();
+            borrowedBooks.remove(book);
+            System.out.println(name + " returned " + book.getTitle());
+        }
+    }    
 
     public String getCustomerId(){
         return customerId;
