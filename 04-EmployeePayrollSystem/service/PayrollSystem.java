5e39e098ef9e0d5c21f0744593101eb791353069
Serge <sajghaa@gmail.com>
2026-03-17 15:31:57 +0200

    simple PayrollSystem.java it's still just simple one

--- 04-EmployeePayrollSystem/service/PayrollSystem.java
+++ 04-EmployeePayrollSystem/service/PayrollSystem.java
@@ -1,5 +1,12 @@
-package 04-EmployeePayrollSystem.service;
+package service;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import model.Employee;
 
 public class PayrollSystem {
-    
+
+    static Scanner sc = new Scanner(System.in);
+    static HashMap<String,Employee> employees =  new HashMap<>();
+
 }
